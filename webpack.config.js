6788c018e641dad9885a81f567bfef844ae34378
Sanapalli Lokesh <slokesh@akamai.com>
2020-11-03 21:10:58 +0530

    added style loader and babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ module.exports = {
     entry: {
         app: "./src/index.js",
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[chunkhash].bundle.js",
