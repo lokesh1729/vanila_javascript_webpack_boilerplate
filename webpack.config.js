5d0079c9eb4169188efc1937c5c82330541f9cb2
Sanapalli Lokesh <slokesh@akamai.com>
2020-11-04 19:12:22 +0530

    added environment variables to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,45 @@ const path = require("path");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const env = "PROD";
 
-module.exports = {
-    entry: {
-        app: './src/index.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: ['babel-loader'],
-                exclude: /(node_modules|bower_components)/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    env === 'DEV'
-                        ? 'style-loader'
-                        : MiniCSSExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
+module.exports = (env) => {
+    return {
+        entry: {
+            app: './src/index.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: ['babel-loader'],
+                    exclude: /(node_modules|bower_components)/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        env && env.NODE_ENV === 'production'
+                            ? MiniCSSExtractPlugin.loader
+                            : 'style-loader',
+                        'css-loader',
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HTMLWebpackPlugin({
+                template: path.resolve(__dirname, 'src', 'index.html'),
+            }),
+            new MiniCSSExtractPlugin({
+                filename: '[name].[chunkhash].css',
+            }),
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        }),
-        new MiniCSSExtractPlugin({
-            filename: '[name].[chunkhash].css',
-        }),
-    ],
-    devServer: {
-        contentBase: './dist',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].bundle.js',
-    },
+        devServer: {
+            contentBase: './dist',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[chunkhash].bundle.js',
+        },
+    }
 }
